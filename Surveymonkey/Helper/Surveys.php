377bbf8e2f46c4a783c4e48e35a7f2c6e70eaefc
jose-wagento <jmt@gmail.com>
2017-11-16 10:28:34 -0400

    create survey helper

--- Surveymonkey/Helper/Surveys.php
+++ Surveymonkey/Helper/Surveys.php
@@ -4,10 +4,57 @@
  */
 namespace Wagento\Surveymonkey\Helper;
 
+use Magento\Framework\App\Helper\Context;
 use Wagento\Surveymonkey\Helper\Api\Api;
 
 class Surveys extends Api {
     // surveymonkey: surveys, pages an questions endpoints
+    /**
+     * endoint for surveys
+     */
+    const SURVEYS = '/v3/surveys';
+    /**
+     * @var \Wagento\Surveymonkey\Helper\Users
+     */
+    private $userName;
 
-    const SURVEYS = '/v3/';
+    public function __construct
+    (
+        Context $context,
+        \Wagento\Surveymonkey\Helper\Data $surveyHelper,
+        \Magento\Framework\HTTP\Client\Curl $client,
+        \Wagento\Surveymonkey\Helper\Users $users
+    )
+    {
+        parent::__construct($context, $surveyHelper, $client);
+        $this->userName = $users->getUserMe();
+    }
+
+    /**
+     * Returns a list of surveys owned or shared with the authenticated user
+     * @return mixed
+     *
+     */
+    public function getSurveys() {
+        $response = $this->get(self::SURVEYS);
+        $data = json_decode($response);
+
+        return $data;
+    }
+
+    public function createEmptySurvey($surveyTitle, $nickname = false) {
+        if($nickname) {
+            $this->userName = $nickname;
+        }
+
+        $params = [
+            'title' => $surveyTitle,
+            'nickname' => $this->userName,
+        ];
+
+        $response = $this->post(self::SURVEYS, $params);
+        $data = json_decode($response, ture);
+
+        return $data;
+    }
 }
