fd24ef72e6b3da32669c5fd2f98f8c57a7d49812
jose-wagento <jmt@gmail.com>
2017-12-05 11:49:38 -0400

    add controller

--- Surveymonkey/Controller/Adminhtml/Survey/Create.php
+++ Surveymonkey/Controller/Adminhtml/Survey/Create.php
@@ -3,20 +3,62 @@
 namespace Wagento\Surveymonkey\Controller\Adminhtml\Survey;
 
 use Magento\Backend\App\Action;
+use Magento\Framework\Controller\ResultFactory;
 
 class Create extends Action {
     /**
      * @var \Wagento\Surveymonkey\Helper\Surveys
      */
-    private $survey;
+    private $surveys;
 
-    public function __construct(Action\Context $context)
+    public function __construct(
+        Action\Context $context,
+        \Wagento\Surveymonkey\Helper\Surveys $surveys
+    )
     {
         parent::__construct($context);
+        $this->surveys = $surveys;
     }
 
+    /**
+     *
+     */
     public function execute()
     {
         // TODO: Implement execute() method.
+
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+
+        $payload = array();
+
+        if (!$this->_formKeyValidator->validate($this->getRequest())) {
+            $this->messageManager->addErrorMessage('Can\' create ticket please try again');
+            return $resultRedirect->setPath('*/*/edit');
+        }
+
+        $data = $this->getRequest()->getParams();
+
+        if(isset($data["general"])){
+            $data = array_merge($data, $data["general"]);
+            unset($data["general"]);
+        }
+
+        $surveyName = trim($data['survey_name']);
+
+        $payload['title'] = $surveyName;
+
+        if (!empty($data['template_id'])) {
+            $payload['from_template_id'] = trim($data['template_id']);
+        }
+
+        if ($survey = $this->surveys->createSurvey($payload)) {
+            $this->messageManager->addSuccessMessage('Ticket Created successfully');
+        }
+        else {
+            $this->messageManager->addErrorMessage('Error while you are creating a new ticket, Please try again');
+        }
+
+        return $resultRedirect->setPath('*/*');
+
     }
 }
